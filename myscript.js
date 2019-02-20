2f6169f59bb40b44b435608a55962cd339762ef7
Martin Rui Andorsen <mndorsen@gmail.com>
2019-02-20 04:33:58 +0100

    Made the function create a 16 x 16 grid of divs covering the site. Moved the css from js to the stylesheet to make the javascript cleaner.

--- myscript.js
+++ myscript.js
@@ -1,36 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-
-	function createSquareRow() {
-		var squareRow = document.createElement("div");
-		squareRow.classList.add("squareRow");
-		squareRow.style.display = "inline-block";
-		squareRow.style.minWidth = "100%";
-		squareRow.style.margin = "0";
-		squareRow.style.padding = "0";
-		squareRow.style.backgroundColor = "Blue";
-		var placement = document.getElementById("container");
-
-		placement.appendChild(squareRow);
-
-		var squarePlacement = document.getElementById("container").getElementsByClassName("squareRow")[0];
-
-		for (var i = 0; i <= 16; i++) {
-			var square = document.createElement("div");
-
-			square.style.width = "5.9%";
-			square.style.height = "10px";
-			square.style.float = "left";
-			
-			console.log(squarePlacement);
-			squarePlacement.appendChild(square);
-		}
-
-
-	}
-
-
-	createSquareRow();
-
-
+	
+	createSquareRow()
 });
+
+
+function createSquareRow() {
+		for (var j = 0; j < 16; j++) {
+			var squareRow = document.createElement("div");
+			squareRow.classList.add("squareRow")
+			var rowPlacement = document.getElementById("container");
+
+			rowPlacement.appendChild(squareRow);
+			
+			for (var i = 0; i < 16; i++) {
+				var square = document.createElement("div");
+				square.classList.add("square")
+				var squarePlacement = document.getElementById("container").getElementsByClassName("squareRow")[j];
+				
+				squarePlacement.appendChild(square)
+			}
+		}
+	}
